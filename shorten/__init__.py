845201a356ab335df29d82a1f8438944e48d3608
PyVolts <pyvolts@gmail.com>
2022-06-03 23:11:11 -0400

    Update __init__.py

--- shorten/__init__.py
+++ shorten/__init__.py
@@ -1,1 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'SUPER SECRET KEY'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    db.init_app(app)
+    from shorten.main.routes import main
+
+    app.register_blueprint(main)
